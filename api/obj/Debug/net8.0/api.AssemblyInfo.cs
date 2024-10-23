5e6e3a233e89df8716731aeeea92321d875b88fc
Ashok Sunuwar <sunuwarashok99@gmail.com>
2024-10-23 08:15:13 +0545

    comment skeleton added

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4853559a4870af2a86dc5d49884067102067d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c6bb53796af68f82e700441e8b8296be8f11f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
